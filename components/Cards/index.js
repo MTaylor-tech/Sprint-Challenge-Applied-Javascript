71b97cd9a30403dc3a425780f0f570fcfecdf6a5
Matthew Taylor <majikpig@gmail.com>
2020-02-23 19:38:02 -0500

    articles

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,47 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const cardsContainer = document.querySelector('.cards-container');
+
+function addArticle (articleObject) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  cardsContainer.appendChild(card);
+
+  const headline = document.createElement('div');
+  headline.classList.add('headline');
+  headline.textContent = articleObject.headline;
+  card.appendChild(headline);
+
+  const authorInfo = document.createElement('div');
+  authorInfo.classList.add('author');
+  card.appendChild(authorInfo);
+
+  const authorImageContainer = document.createElement('div');
+  authorImageContainer.classList.add('img-container');
+  authorInfo.appendChild(authorImageContainer);
+
+  const authorImage = document.createElement('img');
+  authorImage.src = articleObject.authorPhoto;
+  authorImageContainer.appendChild(authorImage);
+
+  const authorCredit = document.createElement('span');
+  authorCredit.textContent = `By ${articleObject.authorName}`;
+  authorInfo.appendChild(authorCredit);
+}
+
+function getArticles () {
+  axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+        .then(function (response) {
+            console.log(response);
+            for (const topic in response.data.articles) {
+              response.data.articles[topic].forEach(article=>addArticle(article));
+            }
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        });
+}
+
+getArticles();
