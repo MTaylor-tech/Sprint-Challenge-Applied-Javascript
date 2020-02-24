7abd9432587c6370d423b51a561df27dea6edda9
Matthew Taylor <majikpig@gmail.com>
2020-02-23 19:55:25 -0500

    started carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,37 @@
     <div class="right-button"> > </div>
   </div>
 */
+const carouselContainer = document.querySelector('.carousel-container');
+
+function addImage (carousel, url) {
+  const image = document.createElement('img');
+  image.src = url;
+  image.style.display = `inline-block`;
+  carousel.appendChild(image);
+}
+
+function makeCarousel () {
+  const carousel = document.createElement('div');
+  carousel.classList.add('carousel');
+  carousel.style.height = `100px`;
+  carouselContainer.appendChild(carousel);
+
+  const leftButton = document.createElement('div');
+  leftButton.classList.add('left-button');
+  leftButton.textContent = ` < `;
+  carousel.appendChild(leftButton);
+
+  addImage(carousel, `./assets/carousel/mountains.jpeg`);
+  addImage(carousel, `./assets/carousel/computer.jpeg`);
+  addImage(carousel, `./assets/carousel/trees.jpeg`);
+  addImage(carousel, `./assets/carousel/turntable.jpeg`);
+
+  const rightButton = document.createElement('div');
+  rightButton.classList.add('right-button');
+  rightButton.textContent = ` > `;
+  carousel.appendChild(rightButton);
+}
+
+makeCarousel();
+const allImages = document.querySelectorAll('.carousel img');
+console.log(allImages);
